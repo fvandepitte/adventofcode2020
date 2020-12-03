b2f93241b2760c2512dcd1c60bfe45fe8c1df30a
Frederiek Vandepitte <fvandepitte@reference.be>
2020-12-03 19:31:44 +0100

    fix day 2 in day 1 part 2

--- AdventOfCode2020.Day1/Program.cs
+++ AdventOfCode2020.Day1/Program.cs
@@ -9,6 +9,51 @@ namespace AdventOfCode2020.Day1
     {
         static async Task Main(string[] args)
         {
+            await Part1();
+            await Part2();
+        }
+
+        private static async Task Part2()
+        {
+            using var reader = new StreamReader(File.OpenRead("input.txt"));
+
+            var numbers = new List<int>();
+            var combos = new List<(int, int)>();
+            var numberFound = false;
+
+            while (!reader.EndOfStream && !numberFound)
+            {
+                var number = int.Parse(await reader.ReadLineAsync());
+
+                foreach (var combo in combos)
+                {
+                    if (combo.Item1 + combo.Item2 + number == 2020)
+                    {
+                        Console.WriteLine(combo.Item1 * combo.Item2 * number);
+                        numberFound = true;
+                        break;
+                    }
+                }
+
+                if (!numberFound)
+                {
+                    foreach (var i in numbers)
+                    {
+                        if (i + number <= 2020)
+                        {
+                            combos.Add((i, number));
+                        }
+                    }
+
+                    numbers.Add(number);
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        private static async Task Part1()
+        {
             using var reader = new StreamReader(File.OpenRead("input.txt"));
 
             var numbers = new List<int>();
--- AdventOfCode2020.Day2/Program.cs
+++ AdventOfCode2020.Day2/Program.cs
@@ -9,41 +9,7 @@ namespace AdventOfCode2020.Day2
     {
         static async Task Main(string[] args)
         {
-            using var reader = new StreamReader(File.OpenRead("input.txt"));
-
-            var numbers = new List<int>();
-            var combos = new List<(int, int)>();
-            var numberFound = false;
             
-            while (!reader.EndOfStream && !numberFound)
-            {
-                var number = int.Parse(await reader.ReadLineAsync());
-
-                foreach (var combo in combos)
-                {
-                    if (combo.Item1 + combo.Item2 + number == 2020)
-                    {
-                        Console.WriteLine(combo.Item1 * combo.Item2 * number);
-                        numberFound = true;
-                        break;
-                    }
-                }
-
-                if (!numberFound)
-                {
-                    foreach (var i in numbers)
-                    {
-                        if (i + number <= 2020)
-                        {
-                            combos.Add((i, number));
-                        }
-                    }
-
-                    numbers.Add(number);
-                }
-            }
-
-            Console.ReadKey();
         }
     }
 }
