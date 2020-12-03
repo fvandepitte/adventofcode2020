adc9b5b08b1c0455c487d4ff8524ee2871b3ac24
Frederiek Vandepitte <fvandepitte@reference.be>
2020-12-03 17:59:12 +0100

    Day 1

---
+++ AdventOfCode2020.Day1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Day1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var reader = new StreamReader(File.OpenRead("input.txt"));
+
+            var numbers = new List<int>();
+            var numberFound = false;
+            while (!reader.EndOfStream && !numberFound)
+            {
+                var number = int.Parse(await reader.ReadLineAsync());
+
+                foreach (var i in numbers)
+                {
+                    if (i + number == 2020)
+                    {
+                        Console.WriteLine(i * number);
+                        numberFound = true;
+                        break;
+                    }
+                }
+
+                numbers.Add(number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
