27e890315356d3549e9ae9ca4c2a31c05370c037
Frederiek Vandepitte <fvandepitte@reference.be>
2020-12-07 10:30:28 +0100

    Day5

---
+++ AdventOfCode2020.Day5/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020.Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            var seatnumbers = lines.Select(x => Parse(x.AsSpan()));
+            Part1(seatnumbers);
+            Part2(seatnumbers);
+        }
+
+        private static void Part1(IEnumerable<int> seatnumbers) 
+        {
+            var result = seatnumbers.Max();
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        private static void Part2(IEnumerable<int> seatnumbers)
+        {
+            var result = Enumerable.Range(0, 1024).Except(seatnumbers).ToList();
+
+            foreach (var seat in result)
+            {
+                if (!(result.Contains(seat - 1) || result.Contains(seat + 1)))
+                {
+                    Console.WriteLine(seat);
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+
+        static int Parse(ReadOnlySpan<char> row)
+        {
+            var result = 0;
+            var muliplier = (int)Math.Pow(2, row.Length - 1);
+            foreach (var c in row)
+            {
+                if (c == 'B' || c == 'R')
+                {
+                    result += muliplier;
+                }
+
+                muliplier /= 2;
+            }
+
+            return result;
+        }
+    }
+}
